b7943151aaf352c59c6df3822abeda5274177ccc
Stroller15 <shubhamverma6351@gmail.com>
2024-07-02 23:11:21 +0530

    build output area skelton

--- src/components/shared/OutputBox.tsx
+++ src/components/shared/OutputBox.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Badge } from "@/components/ui/badge"
+import { BorderBeam } from "../magicui/border-beam"
 
 const OutputBox = () => {
   return (
-    <div>OutputBox</div>
+    <div className="relative flex min-h-[50vh] mt-2 flex-col rounded-xl bg-muted/50 backdrop-blur-sm overflow-hidden border border-primary/5">
+      <BorderBeam size={1200} borderWidth={1.5} duration={4} className="z-10" />
+      <Badge className="absolute top-3 right-3 z-50" variant="outline">Output</Badge>
+
+    </div>
   )
 }
 
